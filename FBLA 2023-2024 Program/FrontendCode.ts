594fcf949c8b3ceab56fad7364e2613fdfa271df
GoldenVolts <blazeplays797@gmail.com>
2024-01-21 18:31:01 -0500

    initial commit

---
+++ FBLA 2023-2024 Program/FrontendCode.ts
@@ -0,0 +1,13 @@
+function Testing()
+{
+    let bruh = 5;
+    var Bruh = 6; //use let to delcare variable, var is old and outdated
+    const bRuh = 7;
+    bruh = Bruh * bruh + bRuh;
+    document.getElementById("test").innerHTML = bruh.toString();
+    return; 
+}
+//single line comment
+/*
+Multi Line Comment
+*/
